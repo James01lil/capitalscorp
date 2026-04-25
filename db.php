6a2e3b885e4075c52fb332ed2fa19427fc30b46b
James01lil <88292602+James01lil@users.noreply.github.com>
2026-04-25 20:34:04 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,19 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=capisdlj_capitalscorp", "capisdlj_capitalscorp", "THEhybrid3002#");
+
+$host = "shuttle.proxy.rlwy.net";
+$port = 58278;
+$db   = "railway";
+$user = "root";
+$pass = "PQryxEVboPKAucKonLsDIqjIKumuPeeE";
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("DB Connection failed: " . $e->getMessage());
+}
 ?>
